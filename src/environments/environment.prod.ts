8de4edbaeae5b8b97ae6e9b818beff1a0ee0174b
nyoumi <nyoumipaul@yahoo.fr>
2021-09-10 21:51:03 +0100

    upper issue

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
+  serverAddress:"  https://dry-temple-95858.herokuapp.com/", 
   firebase: {
     apiKey: "AIzaSyDYv02aLpqUgtJIP9CbT31mOHpu0a77xLM",
     authDomain: "cdk-admin.firebaseapp.com",
@@ -12,3 +13,17 @@ export const environment = {
   },
   mailApi: '/assets/list.json'
 };
+export const palete = {
+  primary: '#D32F2F',
+  accent: '#E65100',
+  warm: '#C2185B',
+  name: '#D50000',
+  secondary: '#D81B60',
+  tertiary: '#8E24AA',
+  quaternary: '#5E35B1',
+  quinary: '#3949AB',
+  secondaryLight: '#E91E63',
+  tertiaryLight: '#9C27B0',
+  quaternaryLight: '#673AB7',
+  quinaryLight: '#3F51B5'       
+}
